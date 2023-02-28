f1741acf10d17be9915692744a5b1b724861a14d
Patrick Lindemann <patrick.lindemann.99@gmail.com>
2023-02-28 21:10:05 +0100

    Revamped email client code

--- src/io/remote/email_client.py
+++ src/io/remote/email_client.py
@@ -2,51 +2,76 @@ import imaplib
 import smtplib
 import ssl
 import time
-from typing import List
+from typing import Dict
+from dataclasses import dataclass
 
-from src.model.config import Config, Server, User
 from src.model.mail import Mail
 
-class EmailClient:
+@dataclass
+class ServerConfig:
+    host: str
+    port: int
+    user: str
+    password: str
 
-    user: User
-    servers: List[Server]
-    imap: any
-    smtp: any
+@dataclass
+class EmailClientWithSSLConfig:
+    imap: ServerConfig
+    smtp: ServerConfig
 
-    def __init__(self, config: Config):
-        self.user = config.user
-        self.servers = config.servers
-        # Connect to the IMAP server
-        try:
-            self.imap = imaplib.IMAP4_SSL(self.servers['imap'].host, self.servers['imap'].port)
-            self.imap.login(self.user.email, self.user.password)
-        except Exception as e:
-            self.close()
-            raise e
-        # Connect to the SMTP server
-        try:
-            context = ssl.create_default_context()
-            self.smtp = smtplib.SMTP_SSL(self.servers['smtp'].host, self.servers['smtp'].port, context=context)
-            self.smtp.login(self.user.email, self.user.password)
-        except Exception as e:
-            self.close()
-            raise e
+class EmailClientWithSSL:
+    config: EmailClientWithSSLConfig
+    connections: Dict[str, any]
+
+    def __init__(self, config: EmailClientWithSSLConfig):
+        self.config = config
+        self.connections = []
+
+    def test_imap(self) -> bool:
+        con = self.__get_imap_connection()
+        return con.check()
+    
+    def test_smtp(self) -> bool:
+        con = self.__get_smtp_connection()
+        return con.check()
+
+    def connect(self):
+        self.__get_imap_connection()
+        self.__get_smtp_connection()
 
     def send(self, mail: Mail):
         self.smtp.sendmail(mail.sender, mail.recipients, mail.as_string())
         self.imap.append('Sent', '\\Seen', imaplib.Time2Internaldate(time.time()), mail.as_string().encode('utf8'))
 
     def close(self):
+        for con in self.connections.values():
+            try:
+                con.close()
+            except:
+                pass
+
+    def __get_imap_connection(self) -> imaplib.IMAP4_SSL:
+        if 'imap' in self.connections:
+            return self.connections['imap']
+        con = imaplib.IMAP4_SSL(self.config.imap.host, self.config.imap.port)
         try:
-            if self.smtp:
-                self.smtp.logout()
-                self.smtp.close()
-        except:
-            pass
+            con.login(self.config.imap.user, self.config.imap.password)
+        except Exception as ex:
+            con.close()
+            raise ex
+        return con
+
+    def __get_smtp_connection(self) -> smtplib.SMTP_SSL:
+        if 'smtp' in self.connections:
+            return self.connections['smtp']
+        con = smtplib.SMTP_SSL(
+            self.config.smtp.host,
+            self.config.smtp.port,
+            context = ssl.create_default_context()
+        )
         try:
-            if self.imap:
-                self.imap.logout()
-                self.imap.close()
-        except:
-            pass
+            con.login(self.config.smtp.user, self.config.smtp.password)
+        except Exception as ex:
+            con.close()
+            raise ex
+        return con
