4759e6e0a53b166c91217c54846d76379f4a974c
Patrick Lindemann <patrick.lindemann.99@gmail.com>
2023-03-02 00:25:45 +0100

    Added utility functions for formatting

---
+++ src/utils/format.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+
+def format_currency(value: float) -> str:
+    return '{:,.2f}€'
+
+def format_date(value: date) -> str:
+    return value.strftime('%d.%m.%Y')
+
+def format_time(value: date) -> str:
+    return value.strftime('%H:%M:%S')
+
+def format_datetime(value: date) -> str:
+    return value.strftime('%d.%m.%Y %H:%M:%S')
