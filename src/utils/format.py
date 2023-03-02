f7cf40fd9ab7c0297fb11c861e4af2513e80c32a
Patrick Lindemann <patrick.lindemann.99@gmail.com>
2023-03-02 11:27:47 +0100

    Changed currency formatting separator

--- src/utils/format.py
+++ src/utils/format.py
@@ -2,7 +2,7 @@ from datetime import date
 
 
 def format_currency(value: float) -> str:
-    return '{:,.2f}€'.format(value)
+    return '{:,.2f}€'.format(value).replace('.', ',')
 
 def format_date(value: date) -> str:
     return value.strftime('%d.%m.%Y')
